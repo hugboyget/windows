1ca88774c7434709291dc244a0471a05da1a78c9
hugboyget <jjf@nyist.edu.cn>
2021-01-31 14:45:12 +0800

    cmd自定义命令行

---
+++ cmd-alias.bat
@@ -0,0 +1,15 @@
+doskey sayhello=echo Hello $*
+doskey cattxt=type xxxxxxxxxxxx.txt $*
+@doskey ls=dir /b $*
+@doskey l=dir /od/p/q/tw $*
+
+::Linux
+
+@doskey cat=type $*
+
+::HugBoy
+
+@doskey me=type cmd-alias.bat
+@doskey whoareyou=echo I am C:\Users\DaFeiDou\cmd-alias.bat, designed by youself.  ____2021-1-31 13:44:41
+@doskey wifi=netsh wlan show profiles  $*  key = clear
+
