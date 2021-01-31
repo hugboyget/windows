78ee042452dc9f414b96767d1b6b978cba54e690
hugboyget <jjf@nyist.edu.cn>
2021-01-31 23:27:57 +0800

    添加常用cmd命令缩写

--- cmd-alias.bat
+++ cmd-alias.bat
@@ -1,5 +1,5 @@
-doskey sayhello=echo Hello $*
-doskey cattxt=type xxxxxxxxxxxx.txt $*
+@doskey sayhello=echo Hello $*
+@doskey cattxt=type xxxxxxxxxxxx.txt $*
 @doskey ls=dir /b $*
 @doskey l=dir /od/p/q/tw $*
 
@@ -9,7 +9,11 @@ doskey cattxt=type xxxxxxxxxxxx.txt $*
 
 ::HugBoy
 
+@echo ######
+@echo  Hello Friend:)
+@echo  Look deep into your heart and find all the wonders in yourself.
 @doskey me=type cmd-alias.bat
 @doskey whoareyou=echo I am C:\Users\DaFeiDou\cmd-alias.bat, designed by youself.  ____2021-1-31 13:44:41
 @doskey wifi=netsh wlan show profiles  $*  key = clear
-
+@doskey green=color 02
+@doskey githelp=echo _____1_____git clone https://www.github.com/hugboyget/Repositories_name.git_____2____git add ._____3_____git commit -m "Descripted about your post"_____4_____ git push origin main
