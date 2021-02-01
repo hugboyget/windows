ca79a44cd06d13bff7faf46d87afd68682a32607
hugboyget <jjf@nyist.edu.cn>
2021-02-01 13:08:43 +0800

    精简代码

--- health-auto.js
+++ health-auto.js
@@ -11,25 +11,19 @@ function clickText(str){
 function health(){ 
     auto.waitFor() 
     var appName = "微信"; 
-    launchApp(appName);
-    toast('健康报送程序启动') // 点击微信 
-   
-    sleep(2000) // 下拉获取小程序目录 
+    launchApp(appName);//打开微信
+    toast('健康报送程序启动') // 启动提示
+    sleep(2000)
     
-    //gestures([500,[300,300],[300,1600]]); //寻找情况报送 
-   
-    //sleep(3000)
-   
-    
+    //打开公众号
     clickText('南工学生工作在线')
     sleep(3000)
     clickText('晨午日报')
     sleep(3000)
     clickText("健康日报")
     sleep(10000)
-    //gesture(500,[300,2000],[300,300])
-    //clickText("提交")
     
+    //尝试提交，直到提交成功
     try{
         clickText("提交")
         sleep(3000)
@@ -37,27 +31,6 @@ function health(){
         sleep(3000)
         clickText("确定")
         }catch(e){}
-    
-//try{
-        //clickText('选择当前体温');
-        //click(380,1376)
-        //sleep(1000);
-        //clickText('确定')
-        //sleep(2000)
-
-        // 选择四类人员
-        //clickText('请选择')
-        
-        //sleep(2000)
-        //clickText('确定')
-        //sleep(2000)
-        //gestures([500,[300,1300],[300,300]]);
-        //sleep(1000);
-
-        // 提交情况报送
-        //clickText('提交')
-        //sleep(2000)
-       // }catch(e){}
 }
-health()
 
+health();
