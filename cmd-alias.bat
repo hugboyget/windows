2391088f884affb3f369c3c756b5375fcdfd65ef
hugboyget <jjf@nyist.edu.cn>
2021-02-01 15:04:36 +0800

    cmd美化调整

--- cmd-alias.bat
+++ cmd-alias.bat
@@ -1,19 +1,23 @@
+::-----------------------------------source-----------------------------
 @doskey sayhello=echo Hello $*
 @doskey cattxt=type xxxxxxxxxxxx.txt $*
 @doskey ls=dir /b $*
 @doskey l=dir /od/p/q/tw $*
-
-::Linux
-
+::-----------------------------------Linux------------------------------
 @doskey cat=type $*
+@doskey vim=$*
+@doskey rm=del $*
 
-::HugBoy
-
+::--------------------------------[Default PATH]------------------------
 @echo ######
 @echo  Hello Friend:)
 @echo  Look deep into your heart and find all the wonders in yourself.
+@echo Default: D:\hugboyget\windows Git  Happy !
+@D:
+@cd hugboyget\windows
+::-----------------------------------HugBoy-----------------------------
 @doskey me=type cmd-alias.bat
-@doskey whoareyou=echo I am C:\Users\DaFeiDou\cmd-alias.bat, designed by youself.  ____2021-1-31 13:44:41
+@doskey whoareyou=echo I am D:\hugboyget\windows\cmd-alias.bat, designed by youself.
 @doskey wifi=netsh wlan show profiles  $*  key = clear
 @doskey green=color 02
-@doskey githelp=echo _____1_____git clone https://www.github.com/hugboyget/Repositories_name.git_____2____git add ._____3_____git commit -m "Descripted about your post"_____4_____ git push origin main
+@doskey githelp=echo [_____1_____]git clone https://www.github.com/hugboyget/Repositories_name.git[_____2____]git add .[_____3_____]git commit -m "Descripted about your post"[_____4_____] git push origin main
